6da0ff9ec3dcb208e8d8a368cc3a7c5d044e5f45
Wm Costantini <costantiniwm@upmc.edu>
2017-03-29 19:26:07 -0400

    new

---
+++ SWList.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+
+
+ships = [
+    "Imperial Star Destroyer",
+    "X-Wing",
+    "TIE Fighter",
+    "Y-Wing",
+    "A-Wing",
+    "B-Wing",
+    "V-Wing",
+    "Mon Calamari Star Cruiser",
+    "YT-400",
+    "Blockade Runner",
+    "Imperial Shuttle",
+    "TIE Bomber",
+    "ETA-2 Jedi Starfighter",
+    "Delta-7 Jedi Starfighter",
+    "Tantive IV",
+    "GR-75 Medium Transport",
+    "Super Star Destroyer",
+    "TIE Interceptor",
+    "TIE Advanced x1",
+    "Zeta-Class Imperial Shuttle",
+    "TIE Striker",
+    "HammerHead Corvette",
+    "ARC-170 STARFIGHTER",
+    "CLONE Z-95 STARFIGHTER",
+    "Fang Fighter",
+    "GEONOSIAN FIGHTER",
+    "GAUNTLET FIGHTER",
+    "GR-75 MEDIUM TRANSPORT",
+    "HH-87 STARHOPPER",
+    "HAVOC MARAUDER",
+    "IMPERIAL INTERDICTOR",
+    "IMPERIAL LIGHT CRUISER",
+    "IMPERIAL LISTENER SHIP"
+]
+
+
+for index in range(len(ships)):
+    print(index,": ",ships[index])
+
+print()
+print("List Completed")
+
+for ship in ships:
+    print("Current Ship: ", ship)
+
+print()
+print("2nd way done")
