a41b48e3ab850f3083ccd0c4f2230ad5b9fd63fe
Wm Costantini <costantiniwm@upmc.edu>
2017-04-03 19:13:03 -0400

    new

---
+++ Challenge7.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+print()
+print()
+# Print each item in the following list:
+# ["The Walking Dead", "Entourage", "The Sopranos", "The Vampire Diaries"]
+
+print("#1")
+print("----------------")
+print()
+
+
+
+print()
+print()
+# Print all the numbers from 25 to 50
+
+print("#2")
+print("----------------")
+print()
+
+
+
+
+print()
+print()
+# Print each item in the list from the first challenge and their indexes
+
+print("#3")
+print("----------------")
+print()
+
+
+
+
+print()
+print()
+# Write a program with an infinite loop (with the option to type q to quit)
+# and a list of numbers. Each time through the loop ask the user to guess a number on the list and
+# tell them whether or not they guessed correctly.
+
+print("#4")
+print("----------------")
+print()
+
+
+
+
+
+print()
+print()
+# Multiply all the numbers in the list [8, 19, 148, 4] with all the numbers in
+# in the list [9, 1, 33, 83], and append each result to a third list.
+
+print("#5")
+print("----------------")
+print()
