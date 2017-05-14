6c7361f134ffe0e5c5c75b9964c07aaedf846d23
Wm Costantini <costantiniwm@upmc.edu>
2017-05-13 21:00:47 -0400

    new

--- Challenge12_3.py
+++ Challenge12_3.py
@@ -13,19 +13,31 @@ print("----------------")
 print()
 
 #------ Methods -------------------------
-
+# Area = ((Height*Base)/2)
+def CalcTrArea(Height,Base):
+    A = ((Height*Base)/2)
+    return A
 
 
 #----------------------------------------
 
 
 class Triangle:
-    def __init__(self, rad, dia, circum):
+    def __init__(self, bas, ht, ang):
         self.base = bas
-        self.side1 = sid1
-        self.side2 = sid2
+        self.height = ht
+        self.angle = ang
         print("Created Triangle!")
 
-Tri1 = Triangle(5,10,25)
-Tri2 = Triangle(15,30,100)
-Tri3 = Triangle(25,50,300)
+Tri1 = Triangle(10,25,90)
+Tri2 = Triangle(15,30,90)
+Tri3 = Triangle(25,50,90)
+
+Set = [Tri1,Tri2,Tri3]
+
+for T in Set:
+    A = CalcTrArea(T.height,T.base)
+    A = str(A)
+    T = str(T)
+    print("Area of " + T + " is " + A)
+    print()
--- Challenge12_4.py
+++ Challenge12_4.py
@@ -10,5 +10,34 @@ print("#4")
 print("----------------")
 print()
 
+import math
+
+#------ Methods -------------------------
+# Perimeter = side * 6
+def calculate_perimeter(side):
+    A = side * 6
+    return A    
+
+#----------------------------------------
+
+class Hexagon:
+    def __init__(self, si):
+        self.side = si
+        print("Created Hexagon Object")
+
+Hexa1 = Hexagon(5)
+Hexa2 = Hexagon(10)
+Hexa3 = Hexagon(15)
+Hexa4 = Hexagon(20)
+Hexa5 = Hexagon(25)
+
+List = [Hexa1, Hexa2, Hexa3, Hexa4, Hexa5]
 
 
+for T in List:
+    A = Hexagon(T.side)
+    A = str(A)
+    T = str(T)
+    print("The perimeter of " + T + " is " + A)
+    print()
+    
